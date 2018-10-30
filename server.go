76f3bc1de360feb44f0a91167e940ead59e5ade8
Vladislav Bolshakov <barbass1025@gmail.com>
2018-10-30 23:17:13 +0600

    Правки

--- server.go
+++ server.go
@@ -3,46 +3,45 @@ package main
 import (
 	"net/http"
 	"io"
-	"fmt"
+	//"fmt"
 	"encoding/json"
 	"database/sql"
     _ "github.com/go-sql-driver/mysql"
     "log"
 )
 
-type Payment_Gateway struct{
-    payment_gateway_id int `json:"payment_gateway_id"`
-    name  string `json:"name"`
-    status int `json:"status"`
+type Payment_Gateway struct {
+    Payment_gateway_id int `json:"payment_gateway_id"`
+    Name  string `json:"name"`
+    Status int `json:"status"`
 }
-var db *sql.DB
+var database *sql.DB
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    res.Header().Set(
-        "Content-Type",
-        "application/json",
-    )
-
-    rows, err := db.Query("SELECT * FROM payment_gateway")
+    rows, err := database.Query("SELECT * FROM hockey_payment.payment_gateway")
     if err != nil {
-        log.Println(err)
+		http.Error(res, http.StatusText(500), 500)
+        return
     }
     defer rows.Close()
-    fmt.Println(rows)
 
-    payment_gateway_list := []Payment_Gateway{}
+    var payment_gateway_list = []Payment_Gateway{}
 
     for rows.Next(){
-        payment_gateway := Payment_Gateway{}
-        err := rows.Scan(&payment_gateway.payment_gateway_id, &payment_gateway.name, &payment_gateway.status)
+        p := Payment_Gateway{}
+        err := rows.Scan(&p.Payment_gateway_id, &p.Name, &p.Status)
         if err != nil{
-            fmt.Println(err)
             continue
         }
-        payment_gateway_list = append(payment_gateway_list, payment_gateway)
+        payment_gateway_list = append(payment_gateway_list, p)
     }
 
     data, err := json.Marshal(payment_gateway_list)
+
+	res.Header().Set(
+        "Content-Type",
+        "application/json",
+    )
 
     io.WriteString(
         res,
@@ -69,13 +68,21 @@ func bye(res http.ResponseWriter, req *http.Request) {
     )
 }
 func main() {
-	db, err := sql.Open("mysql", "root:@127.0.0.1:3306/hockey_payment")
+	db, err := sql.Open("mysql", "root@/hockey_payment")
 	if err != nil {
-        log.Println(err)
+        log.Fatal(err)
     }
     defer db.Close()
 
+    if err = db.Ping(); err != nil {
+        log.Fatal(err)
+    }
+
+    database = db
+
+	log.Println("Run server")
+
     http.HandleFunc("/hello", hello)
     http.HandleFunc("/bye", bye)
-    http.ListenAndServe(":9000", nil)
+    log.Fatal(http.ListenAndServe(":9000", nil))
 }
